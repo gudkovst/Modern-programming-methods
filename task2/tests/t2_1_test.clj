0d439d263c9f2c275299b8989d8ec59cd0afe2e4
gudkovst <69453922+gudkovst@users.noreply.github.com>
2024-09-27 12:36:02 +0800

    update tests 2-1 2-2

--- task2/tests/t2_1_test.clj
+++ task2/tests/t2_1_test.clj
@@ -8,4 +8,4 @@
     (test/is (= ((integrate sin) 0) 0.0))
     (test/is (= ((integrate (fn [x] 5)) 6) 30.0))
     (test/is (<= ((integrate sin) Math/PI) 2))
-    (test/is (= "incorrect argument" ((integrate sin) -1)))))
+    (test/is (= (* -1 ((integrate sin) 1)) ((integrate sin) -1)))))
